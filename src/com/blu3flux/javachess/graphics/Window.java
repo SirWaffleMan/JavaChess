bf0ed4ad8a611f986984d1d873866a1e3373fa79
Abel Acosta <abelacosta1596@gmail.com>
2020-05-28 17:32:43 -0700

    Configured Window class

--- src/com/blu3flux/javachess/graphics/Window.java
+++ src/com/blu3flux/javachess/graphics/Window.java
@@ -1,15 +1,27 @@
 package com.blu3flux.javachess.graphics;
 
+import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Window extends JFrame {
 
 	private static final long serialVersionUID = 1L;
+	private JPanel mainPanel;
 	
 	public Window(int width, int height, String title) {
-		setSize(width, height);
+		// Main Panel configurations
+		mainPanel = new JPanel();
+		mainPanel.setBackground(Color.BLACK);
+		
+		// Set window configurations
+		setPreferredSize(new Dimension(width, height));
 		setTitle(title);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setContentPane(mainPanel);
+		pack();
 		setVisible(true);
 	}
 
