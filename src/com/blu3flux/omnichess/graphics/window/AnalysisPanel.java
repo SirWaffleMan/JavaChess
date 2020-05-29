0d7c38d203eafd928e3e0663c5654749464df2f7
Abel Acosta <abelacosta1596@gmail.com>
2020-05-28 20:33:09 -0700

    Created evaluation bar

--- src/com/blu3flux/omnichess/graphics/window/AnalysisPanel.java
+++ src/com/blu3flux/omnichess/graphics/window/AnalysisPanel.java
@@ -1,15 +1,23 @@
 package com.blu3flux.omnichess.graphics.window;
 
+import java.awt.Dimension;
+
 import javax.swing.JPanel;
 
 public class AnalysisPanel extends JPanel{
 
 	private static final long serialVersionUID = 1L;
 	
+	private EvaluationBar evalBar;
 	private ChessboardPanel chessPanel;
 	
 	public AnalysisPanel() {
+		evalBar = new EvaluationBar();
 		chessPanel = new ChessboardPanel(600);
+		
+		evalBar.setPreferredSize(new Dimension(40, 600));
+		
+		add(evalBar);
 		add(chessPanel);
 	}
 
---
+++ src/com/blu3flux/omnichess/graphics/window/EvaluationBar.java
@@ -0,0 +1,33 @@
+package com.blu3flux.omnichess.graphics.window;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class EvaluationBar extends JPanel{
+	
+	private static final long serialVersionUID = 1L;
+	private double evaluation;
+	
+	public EvaluationBar() {
+		evaluation = 0;
+	}
+	
+	protected void paintComponent(Graphics g) {
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		
+		g.setColor(Color.WHITE);
+		int height = (int) (getHeight() * ((evaluation + 14) / 28.0f));
+		g.fillRect(0, getHeight() - height, getWidth(), height);
+		
+		g.setColor(Color.BLACK);
+		g.drawString(String.valueOf(evaluation), 0, getHeight());
+		
+	}
+	
+	public void setEvaluation(double eval) {
+		this.evaluation = eval;
+	}
+}
