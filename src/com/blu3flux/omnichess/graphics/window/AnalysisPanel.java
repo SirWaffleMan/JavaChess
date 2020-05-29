789acd9b628fa0620fee2626e85e5f1557a80fff
Abel Acosta <abelacosta1596@gmail.com>
2020-05-28 21:54:54 -0700

    Added subpanels

--- src/com/blu3flux/omnichess/graphics/window/AnalysisPanel.java
+++ src/com/blu3flux/omnichess/graphics/window/AnalysisPanel.java
@@ -8,21 +8,34 @@ public class AnalysisPanel extends JPanel{
 
 	private static final long serialVersionUID = 1L;
 	
+	private JPanel leftPanel;
+	private JPanel centerPanel;
+	private JPanel rightPanel;
+	
 	private EvaluationBar evalBar;
 	private ChessBoard chessBoard;
 	private MoveList moveList;
 	
 	public AnalysisPanel() {
+		
+		leftPanel = new JPanel();
+		centerPanel = new JPanel();
+		rightPanel = new JPanel();
+		
 		evalBar = new EvaluationBar();
 		chessBoard = new ChessBoard(600);
 		moveList = new MoveList();
 		
+		leftPanel.add(evalBar);
+		centerPanel.add(chessBoard);
+		rightPanel.add(moveList);
+		
 		evalBar.setPreferredSize(new Dimension(40, 600));
 		moveList.setPreferredSize(new Dimension(300, 600));
 
-		add(evalBar);
-		add(chessBoard);
-		add(moveList);
+		add(leftPanel);
+		add(centerPanel);
+		add(rightPanel);
 	}
 
 }
