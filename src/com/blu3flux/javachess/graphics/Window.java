0009da497d55b9e30cfde774f46ef42042e58e4f
Abel Acosta <abelacosta1596@gmail.com>
2020-05-28 17:49:38 -0700

    Added analysis tab

--- src/com/blu3flux/javachess/graphics/Window.java
+++ src/com/blu3flux/javachess/graphics/Window.java
@@ -5,22 +5,33 @@ import java.awt.Dimension;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 public class Window extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel mainPanel;
+	private JTabbedPane tabPanel;
+	private JPanel playPanel;
+	private JPanel analysisPanel;
 	
 	public Window(int width, int height, String title) {
-		// Main Panel configurations
-		mainPanel = new JPanel();
-		mainPanel.setBackground(Color.BLACK);
+		// Tab panel configurations
+		playPanel = new JPanel();
+		analysisPanel = new JPanel();
+		
+		tabPanel = new JTabbedPane();
+		tabPanel.addTab("Play", playPanel);
+		tabPanel.addTab("Analysis", analysisPanel);
+		for (int i = 0; i < tabPanel.getTabCount(); i++) {
+			tabPanel.getComponentAt(i).setBackground(Color.DARK_GRAY);
+		}
 		
 		// Set window configurations
 		setPreferredSize(new Dimension(width, height));
 		setTitle(title);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setContentPane(mainPanel);
+		setContentPane(tabPanel);
 		pack();
 		setVisible(true);
 	}
