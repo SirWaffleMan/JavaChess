32c510ae10a5bd8cedf80e292d7a3ae0408a9d97
Abel Acosta <abelacosta1596@gmail.com>
2020-06-10 18:21:26 -0700

    Added comment

--- src/com/blu3flux/omnichess/utils/ChessGameValidator.java
+++ src/com/blu3flux/omnichess/utils/ChessGameValidator.java
@@ -1,5 +1,10 @@
 package com.blu3flux.omnichess.utils;
 
 public class ChessGameValidator {
-
+	
+	// Check if chess move is valid in chess position
+	public boolean checkValidMove(String fen, String move) {
+		//TODO: Check if move is valid in given fen position
+		return true;
+	}
 }
