2f61fe4a0e7b2bd6359cbe95021b457c9a2c6cd0
Abel Acosta <abelacosta1596@gmail.com>
2020-11-26 16:37:46 -0800

    Created class to validate FEN strings

---
+++ src/com/blu3flux/omnichess/utils/FENValidator.java
@@ -0,0 +1,9 @@
+package com.blu3flux.omnichess.utils;
+
+public class FENValidator {
+
+    //TODO: FINISH THIS METHOD
+    public static boolean checkValidFEN(String fen){
+        return true;
+    }
+}
